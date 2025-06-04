37990103eb65ceae9a563938d90424d2622b3d14
HoneyWater8 <kjyjyh8@gmail.com>
2025-06-04 15:28:05 +0900

    DB 주석 해제

--- django-server/config/settings.py
+++ django-server/config/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'topDB',    # 데이터베이스 이름.
-#         "USER": "topAdmin",  # 사용자 이름.
-#         "PASSWORD": "root1234",    # 비밀번호
-#         "HOST": "127.0.0.1",    # 호스트명
-#         "PORT": "5432"          # 포트번호
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'topDB',    # 데이터베이스 이름.
+        "USER": "topAdmin",  # 사용자 이름.
+        "PASSWORD": "root1234",    # 비밀번호
+        "HOST": "127.0.0.1",    # 호스트명
+        "PORT": "5432"          # 포트번호
+    }
+}
 
 
 # Password validation
