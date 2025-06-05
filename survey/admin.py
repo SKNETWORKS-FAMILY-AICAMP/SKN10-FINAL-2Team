2d46bec5e8eec575426ab9acd29138d8ecfaabd0
Seo_Droid <seo.kwonski@gmail.com>
2025-06-05 14:51:00 +0900

    Usunięcie plików aplikacji Survey, w tym modeli, widoków, tras URL oraz plików konfiguracyjnych. Zlikwidowano również pliki szablonów HTML oraz arkusze stylów CSS związane z aplikacją.

--- survey/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import SurveyResponse
-
-@admin.register(SurveyResponse)
-class SurveyResponseAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'sedentary', 'gender', 'birth_year')
-    list_filter = ('sedentary', 'gender', 'created_at')
-    search_fields = ('gender', 'birth_year')
-    ordering = ('-created_at',) 
--- survey/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class SurveyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'survey'
-    verbose_name = '건강 설문조사' 
