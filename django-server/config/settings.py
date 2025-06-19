6d583ec63a48fbbeaedfd9e4601144d5954fdfa8
uhyeong <oneyou1112@naver.com>
2025-06-20 00:09:35 +0900

    host allowed

--- django-server/config/settings.py
+++ django-server/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-v&$=%4dhpx&^khwr0=-xlyys@a9%=aw4$e&rqih!-9hz-#6-_r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
