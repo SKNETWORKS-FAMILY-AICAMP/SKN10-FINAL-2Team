bdf282a3b636bc5d9b98896195f3cdb1e924af6d
redwin-02 <tmdvy2572@gmail.com>
2025-06-19 10:19:38 +0900

    메인페이지 수정

--- django-server/Product/urls.py
+++ django-server/Product/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('details/<int:product_id>/', views.get_product_details, name='product_details'),
     path('details/', views.get_product_details, name='product_details_bulk'),
+    path('get_weighted_scores/', views.get_weighted_scores, name='get_weighted_scores'),
+    path('get_popular_products/', views.get_popular_products, name='get_popular_products'),
+    path('get_best_selling_products/', views.get_best_selling_products, name='get_best_selling_products'),
 ] 
